3f6415d7a89cad12f7917ae6dca6f1a3f791ac6e
joncraigdesign <38989712+joncraigdesign@users.noreply.github.com>
2024-07-17 16:16:25 -0400

    Desktop

---
+++ index.php
@@ -0,0 +1,232 @@
+<?php
+ include('template/header.php'); 
+?>
+
+<div class="hero">
+<div class="hero-b">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-7 col-sm-offset-5 adj-1">
+				<img src="img/aso.webp" class="img-responsive" />
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-7 col-sm-offset-4 pos">
+				<h1 class="adj-2 text-center">MoneyBox</h1>
+				<h2 class="text-center">This Box Makes You <u>Money</u></h2>
+				<div class="adj-3">
+					<img src="img/box.webp" class="img-responsive" />
+					<div class="cta">
+						<a href="#" class="cta-btn">Get My Moneybox Now!</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+<div class="hero-floor">
+	<div class="container pos">
+		<div class="row adj-1">
+			<div class="shirt-con">
+				<img src="img/cut-shirt.webp" class="img-responsive" />
+			</div>
+			<div class="col-sm-4 col-sm-offset-4">
+				<ul class="checks">
+					<li><span class="cw-bold">E-book:</span> Zero to Seven Figures Online</li>
+					<li><span class="cw-bold">E-book:</span> How to Steal Online Secrets</li>
+					<li><span class="cw-bold">Motivational Merch:</span> Stay Motivated with Special MERCH</li>
+				</ul>
+			</div>
+			<div class="col-sm-4">
+				<ul class="checks">
+					<li><span class="cw-bold">Playbook:</span> Seven Figure E-Com</li>
+					<li><span class="cw-bold">Full Book:</span> How to Make Money and Influence Customers</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="red-bar">
+	<span class="white">Free</span> &nbsp;Discord Membership&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="white">Free</span> &nbsp;9 Module Course&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="white">Free</span> &nbsp;Webinar Access&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="white">Free</span> &nbsp;Community Access
+</div>
+<div class="tier-2 pos">
+<div class="tier-2b"></div>
+	<div class="half-circ"><img src="img/bg-2-tl.webp" class="img-responsive" /></div>
+	<div class="container pos">
+		<div class="book bk-a">
+			<img src="img/book-1.webp" class="img-responsive" />
+		</div>
+		<div class="row adj-1">
+			<div class="col-sm-5 col-sm-offset-1 blurb">
+				<div class="title-con pos">
+					<img src="img/hard-copy.webp" class="img-responsive" />
+				</div>
+				<h4>Zer<span class="red">o</span> to <span class="red">7</span> Figures Online</h4>
+				<p>Learn how to...</p>
+				<ul class="circles adj-2">
+					<li>Build a brand <span class="red cw-bold">from scratch</span></li>
+					<li>Identify <span class="red cw-bold">high performing</span> brands</li>
+					<li>Replicate their <span class="red cw-bold">strategies</span></li>
+					<li>Unlock <span class="red cw-bold">growth hacks</span> for success</li>
+					<li>Identify what works <span class="red cw-bold">online</span></li>
+					<li>Use this information and build your <span class="red cw-bold">own empire</span></li>
+				</ul>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-10 col-sm-offset-2 red-bar right">
+				<p>
+				“...one small action can set off a chain reaction, <br />the person who does something will stomp <br />the person who doesn’t...”
+				</p>
+			</div>
+		</div>
+		<div class="book bk-b">
+			<img src="img/book-2.webp" class="img-responsive" />
+		</div>
+		<div class="row adj-3">
+			<div class="col-sm-5 col-sm-offset-1 blurb">
+				<div class="title-con pos">
+					<img src="img/hard-copy.webp" class="img-responsive" />
+				</div>
+				<h4 class="sml">How to Make <br /><span class="red">Money</span> & <span class="red">Influence</span> Customers</h4>
+				<p>By Liam Christopher...</p>
+				<ul class="circles adj-2">
+					<li>How to build a <span class="red cw-bold">winning mindset</span></li>
+					<li>How to create <span class="red cw-bold">winning circles</span></li>
+					<li>How to find a brand <span class="red cw-bold">suited to you</span></li>
+					<li>How to <span class="red cw-bold">split test</span> strategies</li>
+					<li>How to find and copy <span class="red cw-bold">winning ideas</span></li>
+					<li>How to build a <span class="red cw-bold">winning team</span></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="sep">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6 col-sm-offset-3 cta">
+				<a href="#" class="cta-btn">Get My Moneybox Now!</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="tier-3">
+	<div class="container pos">
+		<div class="book bk-c">
+			<img src="img/book-3.webp" class="img-responsive" />
+		</div>
+		<div class="row adj-1">
+			<div class="col-sm-5 col-sm-offset-6 blurb">
+				<div class="title-con pos">
+					<img src="img/hard-copy.webp" class="img-responsive" />
+				</div>
+				<h4>How to <span class="red">Steal</span> Online Secrets</h4>
+				<p>Learn how to...</p>
+				<ul class="circles adj-2">
+					<li>Identify brands that are <span class="red cw-bold">crushing</span> it</li>
+					<li>Understand <span class="red cw-bold">what drives</span> them</li>
+					<li>How to identify online <span class="red cw-bold">trends</span></li>
+					<li>How to find <span class="red cw-bold">influencer promo's</span></li>
+					<li>Repurpose <span class="red cw-bold">successful content</span></li>
+					<li>Learn about <span class="red cw-bold">'growth hacking'</span></li>
+				</ul>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-10 red-bar left">
+				<p>
+				“...one small action can set off a chain reaction, <br />the person who does something will stomp <br />the person who doesn’t...”
+				</p>
+			</div>
+		</div>
+		<div class="book bk-d">
+			<img src="img/book-4.webp" class="img-responsive" />
+		</div>
+		<div class="row adj-3">
+			<div class="col-sm-5 col-sm-offset-6 blurb">
+				<div class="title-con pos">
+					<img src="img/hard-copy.webp" class="img-responsive" />
+				</div>
+				<h4 class="sml">Seven Figure <br /><span class="red">Ecom</span> Playbook</h4>
+				<p>This book comes with a free 9 <br />module digital course where <br />you will learn how to...</p>
+				<ul class="circles adj-2">
+					<li><span class="red cw-bold">Split test</span> winning ideas</li>
+					<li><span class="red cw-bold">Create</span> a customer experience</li>
+					<li><span class="red cw-bold">Reverse engineer</span> what works</li>
+					<li><span class="red cw-bold">Scale</span>, monetise and sell</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="tier-4">
+	<div class="container pos">
+		<div class="shirt"><img src="img/shirt.webp" class="img-responsive" /></div>
+		<div class="row">
+			<div class="col-sm-6 col-sm-offset-1 adj-1">
+				<img src="img/motivational.webp" class="img-responsive" />
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-5 col-sm-offset-2 blurb">
+				<p>If you’re gonna build cool brands and make money online you are gonna have to stay motivated.  Each kit includes:</p>
+				<ul class="circles adj-2">
+					<li><span class="red cw-bold">100%</span> Cotton fitted unixes shirt</li>
+					<li><span class="red cw-bold">Trademarked</span> hustle direct print</li>
+					<li><span class="red cw-bold">Ethically</span> sourced material</li>
+					<li><span class="red cw-bold">Sustainably</span> packaged</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="tier-5">
+	<div class="container">
+		
+		<div class="row">
+			<div class="col-xs-4 col-sm-offset-3">
+				<img src="img/tes-1.webp" class="img-responsive" />
+			</div>
+			<div class="col-xs-8 col-sm-5">
+				<p><span class="red">Lorem ipsum dolor sit amet, consectetur adipiscing elit,</span> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-8 col-sm-5">
+				<p><span class="red">Lorem ipsum dolor sit amet, consectetur adipiscing elit,</span> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+			</div>
+			<div class="col-xs-4">
+				<img src="img/tes-2.webp" class="img-responsive" />
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-xs-4 col-sm-offset-3">
+				<img src="img/tes-3.webp" class="img-responsive" />
+			</div>
+			<div class="col-xs-8 col-sm-5">
+				<p><span class="red">Lorem ipsum dolor sit amet, consectetur adipiscing elit,</span> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-8 col-sm-5">
+				<p><span class="red">Lorem ipsum dolor sit amet, consectetur adipiscing elit,</span> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+			</div>
+			<div class="col-xs-4">
+				<img src="img/tes-4.webp" class="img-responsive" />
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php
+ include('template/footer.php'); 
+?>
---
+++ template/footer.php
@@ -0,0 +1,44 @@
+<style>
+	footer {padding:50px 15px;}
+	footer p, footer li {font-size:16px;}
+	footer a {color:#e63444;}
+</style>
+
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4">
+				<p><b>Quick Links:</b></p>
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Free course</a></li>
+					<li><a href="#">Free e-book</a></li>
+					<li><a href="#">Full course</a></li>
+					<li><a href="#">Affiliates</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</div>
+			<div class="col-sm-4">
+				<p><b>Head offices:</b><br />
+				Sydney | Los Angeles | Miami<br />
+				Working hours:<br />
+				We never sleep!</p>
+
+				<p><b>Contact us:</b><br />
+				<a href="mailto:hello@itsliamchristopher.com">hello@itsliamchristopher.com</a><br />
+				<a href="mailto:liam@fearlessmedialabs.com">liam@fearlessmedialabs.com</a><br /></p>
+			</div>
+			<div class="col-sm-4">
+				<p><b>Disclaimer:</b> <br />Liam Christopher and all affiliated brands, agency groups, consultants and specialists work around the clock to provide first class customer service to every valued customer and client we touch. If you put the time and effort in to reach out, we will exceed your expectations in our response.</p>
+			</div>
+		</div>
+	</div>
+</footer>
+</div>
+</body>
+<script src="js/wow.js"></script>
+  <script>
+  new WOW().init();
+  </script>
+</html>
